78b7c828b0e1be73ce213269b7d0bfe146315cf7
Dennis Boguslavskiy <dennisboguslavskiy@denniss-air.lan>
2023-10-30 00:00:20 -0400

    setup database, factory, seed

--- myapp/myapp/models.py
+++ myapp/myapp/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name
+    
+class Course(models.Model):
+    course_name = models.CharField(max_length=100)
+    course_section = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.course_name
