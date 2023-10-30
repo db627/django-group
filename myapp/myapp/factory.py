9b1dd9b8d4075b3718cf46951b8ca2a97c4b7feb
Dennis Boguslavskiy <dennisboguslavskiy@denniss-air.lan>
2023-10-30 00:01:06 -0400

    setup factory and management

---
+++ myapp/myapp/factory.py
@@ -0,0 +1,20 @@
+import factory
+from faker import Faker
+from myapp.models import User, Course
+
+
+fake = Faker()
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+    
+    first_name = factory.LazyAttribute(lambda x: fake.first_name())
+    last_name = factory.LazyAttribute(lambda x: fake.last_name())
+
+class CourseFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Course
+    
+    course_name = factory.LazyAttribute(lambda x: fake.bs())  # e.g. "Synergized modular task-force"
+    course_section = factory.LazyAttribute(lambda x: fake.bothify(text="Section ##"))  # e.g. "Section 12"
